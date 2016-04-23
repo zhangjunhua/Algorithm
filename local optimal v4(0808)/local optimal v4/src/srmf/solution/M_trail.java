d469e0408a9a2b03a1dc8300ca781ede4cfca53e
ylx <742805630@qq.com>
2016-04-23 14:37:34 +0800

    构建了m_Trail类

--- local optimal v4(0808)/local optimal v4/src/srmf/solution/M_trail.java
+++ local optimal v4(0808)/local optimal v4/src/srmf/solution/M_trail.java
@@ -1,7 +1,20 @@
 package srmf.solution;
 
+import java.util.ArrayList;
+
+import srmf.network.Link;
+
 
 
 public class M_trail {
+public ArrayList<Link> links=new ArrayList<Link>();
+
+public ArrayList<Link> getLinks() {
+	return links;
+}
+
+public void setLinks(ArrayList<Link> links) {
+	this.links = links;
+}
 
 }
